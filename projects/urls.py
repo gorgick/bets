4bd6a8c9a83e7521016749b02a6724d123572269
gorgick <gorgick2014@yandex.by>
2024-05-20 19:15:55 +0300

    DetailView

--- projects/urls.py
+++ projects/urls.py
@@ -1,10 +1,14 @@
 from django.urls import path
 
-from projects.views import ProjectsListView
+from projects.views import (
+    ProjectsListView,
+    ProjectDetailView
+)
 
 app_name = 'projects'
 
 urlpatterns = [
     path('projects/', ProjectsListView.as_view(), name='projects'),
+    path('projects/<int:pk>/', ProjectDetailView.as_view(), name='project_detail'),
 ]
 
--- projects/views.py
+++ projects/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
 from django.views import View
+from django.views.generic import DetailView
 
 from projects.models import Project
 
@@ -8,3 +9,9 @@ class ProjectsListView(View):
     def get(self, request, *args, **kwargs):
         products = Project.objects.all()
         return render(request, 'projects/projects.html', {'products': products})
+
+
+class ProjectDetailView(DetailView):
+    model = Project
+    template_name = 'projects/project_detail.html'
+    context_object_name = 'product'
