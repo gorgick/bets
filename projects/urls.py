2d2dd4f4398134911e828788560917f8212a6afa
gorgick <gorgick2014@yandex.by>
2024-05-21 19:16:21 +0300

    CartMixin, CartView

--- projects/urls.py
+++ projects/urls.py
@@ -2,13 +2,15 @@ from django.urls import path
 
 from projects.views import (
     ProjectsListView,
-    ProjectDetailView
+    ProjectDetailView,
+    CartView
 )
 
 app_name = 'projects'
 
 urlpatterns = [
     path('projects/', ProjectsListView.as_view(), name='projects'),
+    path('cart/', CartView.as_view(), name='cart'),
     path('projects/<int:pk>/', ProjectDetailView.as_view(), name='project_detail'),
 ]
 
--- projects/views.py
+++ projects/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render
 from django.views import View
 from django.views.generic import DetailView
 
+from projects.mixins import CartMixin
 from projects.models import Project
 
 
@@ -15,3 +16,8 @@ class ProjectDetailView(DetailView):
     model = Project
     template_name = 'projects/project_detail.html'
     context_object_name = 'product'
+
+
+class CartView(CartMixin, View):
+    def get(self, request, *args, **kwargs):
+        return render(request, 'projects/cart.html', {'cart': self.cart})
